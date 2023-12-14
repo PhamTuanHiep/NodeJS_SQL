0d0f475de305bed9464839ecab717db918ddefbc
Tuan Hiep <tuanhiep@gmail.com>
2023-12-14 15:23:29 +0700

    istall, create table, add  data in database with sequelize cli

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NodeJS_SQL
+
+npm start
+localhost:8080
+xampp-mysql: http://localhost/phpmyadmin/
---
+++ src/seeders/20231214073543-demo-user.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  //add data
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "example@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  //undoing ↔ back to old data which havent’t been faulty
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
