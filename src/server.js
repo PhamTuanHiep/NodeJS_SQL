3b6790b5f8867794a8e1ef1b95d127d5806d645e
Tuan Hiep <tuanhiep@gmail.com>
2023-12-14 16:50:05 +0700

    connect to mySql database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+
 require("dotenv").config(); // run line: port = precess.....
 
 let app = express();
@@ -14,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
 //Port === undefinend -> port= 6969
 let port = process.env.PORT || 6969;
 
