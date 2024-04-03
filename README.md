fee09a57ca22f466b53babb1305a71464093297a
Tuan Hiep <tuanhiep@gmail.com>
2024-04-03 11:00:21 +0700

    25-CRUD-show inf -step1

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ localhost:8080
 xampp-mysql: http://localhost/phpmyadmin
 update database: npx sequelize-cli db:migrate
 update data: npx sequelize-cli db:seed:all
+// "start": "nodemon --exec ./node_modules/.bin/babel-node src/server.js"
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
@@ -10,7 +11,17 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log("message:", message);
+  return res.send("post crud from server");
+};
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage,
+  getCRUD,
+  postCRUD,
 };
