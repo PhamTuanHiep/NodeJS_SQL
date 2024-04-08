2e57cad21fde8d76a9a078016ea5b5aa174ef617
Tuan Hiep <tuanhiep@gmail.com>
2024-04-09 00:15:52 +0700

    27- CRUD (Edit)- step2

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -19,9 +19,40 @@ let postCRUD = async (req, res) => {
   console.log("message:", message);
   return res.send("post crud from server");
 };
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  return res.render("displayCRUD.ejs", { dataTable: data });
+};
+let getEditCRUD = async (req, res) => {
+  console.log("req.query:", req.query);
+
+  let userId = req.query.id;
+  console.log(userId);
+
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    console.log("------------------------------------");
+    console.log(userData);
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not found");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUsers,
+  });
+};
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage,
   getCRUD,
   postCRUD,
+  displayGetCRUD,
+  getEditCRUD,
+  putCRUD,
 };
